5f0a4fb9dc2b8c52a2468fe4c0968c520176f735
Venum <venums46@gmail.com>
2023-04-16 20:01:23 +0300

    1.1

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import flask
-from models.product import Product
-
+# from models.product import Product
+import requests
+from bs4 import BeautifulSoup
+import lxml
 
 app = flask.Flask(__name__)
 
-@app.route("/product")
-def product_page():
-    products: list[Product]
-    return flask.render_template('product.html', products=products)
+URL = 'https://karaganda.emenu.delivery/restoran/today_cafe_krg/menu/29951'
 
+
+@app.route("/product", methods=['GET','POST'])
+def productes_page():
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.text, "lxml-xml")
+    names = soup.find_all('div', class_='item-title')
+    names_breakfast: list[str] = []
+
+    for name in names:
+        names_breakfast.append(name.text.strip())
+    
+    return flask.render_template(
+        template_name_or_list='product.html',
+        products = names_breakfast
+    )
 
 if __name__ == '__main__':
     app.run(
--- models/product.py
+++ models/product.py
@@ -1,29 +1,89 @@
 import requests
 from bs4 import BeautifulSoup
+from lxml import etree as et
 
 
-URL = 'https://glovoapp.com/kz/ru/karaganda/today-kgf-kgf/'
+# URL = 'https://glovoapp.com/kz/ru/karaganda/today-kgf-kgf/'
 
-response = requests.get(URL)
-soup = BeautifulSoup(response.text, "html.parser")
+# class Product:
 
+URL = 'https://karaganda.emenu.delivery/restoran/today_cafe_krg/menu/29951'
 
 class Product:
-    def product_page():
-        names = soup.find_all('div', class_='product-row__name')
-        structure = soup.find_all('span', class_='product-row__info__description')
-        prices = soup.find_all(
-            'div', 
-            class_='product-price product-row__price layout-vertical-tablet'
-            )
-        
-        list_product: list[str] = []
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.text, "lxml-xml")
+    names = soup.find_all('div', class_='item-title')
+    names_breakfast: list[str] = []
 
-        for i in range(len(names)):
-            current_name = names[i].text.strip()
-            current_structure = structure[i].text.strip()
-            current_prices = prices[i].text.strip()
-            products = current_name, current_structure, current_prices
-            list_product.append(products)
+    for name in names:
+        names_breakfast.append(name.text.strip())
 
-        return list_product
+
+# class Product:
+#     names = soup.find_all('div', class_='product-row__name')
+#     structure = soup.find_all('span', class_='product-row__info__description')
+#     prices = soup.find_all(
+#         'div', 
+#         class_='product-price product-row__price layout-vertical-tablet'
+#     )
+
+
+#     print(names.text.strip())
+    # names: list[str] = []
+    # structure: list[str] = []
+    # prices: list[str] = []
+
+    # for name in names:
+    #     names.append(name.text.strip())
+    
+    # for struct in structure:
+    #     names.append(struct.text.strip())
+
+    # for price in prices:
+    #     names.append(price.text.strip())
+
+
+    # for i in range(len(structure)):
+    #     price(f'''
+    #     name: {names[i]}
+    #     structure: {structure[i]}
+    #     price: {prices[i]}
+    #     ''')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # print(structure)
+        # print(prices)
+        # names: list[str] = []
+        # structure: list[str] = []
+        # prices: list[str] = []
+        # product_list = list[names,structure, prices]
+
+        # for i in range(len(structure)):
+        #     current_name = names[i].text.strip()
+        #     current_structure = structure[i].text.strip()
+        #     current_prices = prices[i].text.strip()
+        #     products = current_name, current_structure, current_prices
+
+        # prod = Product(
+        #     name=current_name,
+        #     structure=current_structure,
+        #     price=current_prices
+        # )
+        # product_list.append(prod)
+
+        # return product_list
