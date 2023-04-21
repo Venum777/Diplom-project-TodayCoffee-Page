cf04814b671251d60e3a0c201aa3e4bb5fd0f2e0
Venum <venums46@gmail.com>
2023-04-21 12:17:12 +0300

    Page beta 0.4

--- app.py
+++ app.py
@@ -2,17 +2,48 @@ import flask
 from models.product import Product
 import requests
 from bs4 import BeautifulSoup
-import lxml
 
-app = flask.Flask(__name__)
 
 URL = 'https://karaganda.emenu.delivery/restoran/today_cafe_krg/menu/29951'
 
+app = flask.Flask(__name__)
+products: list[Product] = []
+
 
 @app.route("/products", methods=['GET','POST'])
-def productes_page():
-    products: list[Product] = Product.get_products()
-    return products
+def get_products():
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.text, "lxml")
+    list_breakfast: list = soup.find_all('div', class_='list-product-item')
+
+    list_names_breakfast: list[str] = []
+    list_structure_breakfast: list[str] = []
+    list_prices_breakfast: list[str] = []
+
+    for tag1 in list_breakfast:
+        names_breakfast = (tag1.find('div', class_='item-title').text)
+        list_names_breakfast.append(names_breakfast)
+
+    for tag2 in list_breakfast:
+        structure_breakfast = (tag2.find('p', class_='hidden-lg').text.replace("\n",""))
+        list_structure_breakfast.append(structure_breakfast)
+
+    for tag3 in list_breakfast:
+        prices_breakfast = (tag3.find('span', class_='price').text)
+        list_prices_breakfast.append(prices_breakfast)
+
+    product = Product(
+        name=list_names_breakfast,
+        structure=list_structure_breakfast,
+        price=list_prices_breakfast
+    )
+    products.append(product)
+        
+    return flask.render_template(
+        template_name_or_list="product.html",
+        products=products
+    )
+
 
 if __name__ == '__main__':
     app.run(
--- models/product.py
+++ models/product.py
@@ -1,62 +1,11 @@
-import flask
-import requests
-from bs4 import BeautifulSoup
-from lxml import etree as et
-
-
-URL = 'https://karaganda.emenu.delivery/restoran/today_cafe_krg/menu/29951'
-
 class Product:
-    @staticmethod
-    def get_products():
-        response = requests.get(URL)
-        soup = BeautifulSoup(response.text, "lxml")
-        names = soup.find_all('div', class_='item-title')
-        names_breakfast: list[str] = []
-
-        for name in names:
-            names_breakfast.append(name.text.strip())
-        
-        return flask.render_template(
-            template_name_or_list='product.html',
-            products = names_breakfast
-        )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # print(structure)
-        # print(prices)
-        # names: list[str] = []
-        # structure: list[str] = []
-        # prices: list[str] = []
-        # product_list = list[names,structure, prices]
-
-        # for i in range(len(structure)):
-        #     current_name = names[i].text.strip()
-        #     current_structure = structure[i].text.strip()
-        #     current_prices = prices[i].text.strip()
-        #     products = current_name, current_structure, current_prices
-
-        # prod = Product(
-        #     name=current_name,
-        #     structure=current_structure,
-        #     price=current_prices
-        # )
-        # product_list.append(prod)
-
-        # return product_list
+    def __init__(
+        self,
+        name: str,
+        structure: str,
+        price: int    
+    ) -> None:
+        self.name = name
+        self.structure = structure
+        self.price = price
+    
